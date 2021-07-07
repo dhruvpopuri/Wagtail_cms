e3ff8369c87e3491b6fd6955758aefeabcd02291
P Sai Dhruv <saidhruv03@gmail.com>
2021-07-07 18:38:48 +0530

    initial commit

---
+++ dvm_cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure---xwb*a)yozpgmb-^1rmb%qp(=c+#ho(t&9d+kdrk4q-8#4_!y'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
